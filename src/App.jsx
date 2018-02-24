95882afc0b9981250f46f7386bde07b85f530470
Tommy Spielhoelle <spiel.hoelle@live.de>
2018-02-24 15:33:12 +0100

    WIP render product list and add buttons

--- src/App.jsx
+++ src/App.jsx
@@ -11,27 +11,45 @@ class App extends React.Component {
           "id":1,
           "name":"JavaScript: The Definitive Guide, 6th Edition",
           "release": "September 2010",
+          "amount": 1,
           "price":"2.99"
         },
         {
           "id":2,
           "name":"Ruby on Rails: Up and Running",
           "release": "March 2007",
+          "amount": 1,
           "price":"30.99"
         },
         {
           "id":3,
           "name":"MongoDB: The Definitive Guide",
           "release": "Januar 2019",
+          "amount": 1,
           "price":"99.99"
-        }
+        },
+                {
+          "id":4,
+          "name":"Linux Cookbook",
+          "release": "Februar 2009",
+          "amount": 1,
+          "price":"24.99"
+        },
+
       ]
     }
+
+    this.handleItem = this.handleItem.bind(this);
+
   };
+  handleItem(item){
+    console.log(item.target)
+    //this.setState({ posts })
+  }
   render() {
     return (
       <div>
-      {this.state.data.map((person, i) => <Product key = {i} data = {person} />)}
+      {this.state.data.map((person, i) => <Product handleItem={this.handleItem} key = {i} data = {person} />)}
     </div>
     );
   }
@@ -40,6 +58,9 @@ class Product extends React.Component {
   render() {
     return (
       <li className="list-group-item d-flex justify-content-between lh-condensed">
+        <button className="btn btn-sm btn-light" onClick = {this.props.handleItem}>+</button>
+        <button className="btn btn-sm btn-light" onClick = {this.props.handleItem}>-</button>
+
         <div>
           <h6 className="my-0">{this.props.data.name}</h6>
           <small className="text-muted">Release: { this.props.data.release }</small>
