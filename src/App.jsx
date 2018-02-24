0c4ff147420c6b7882b8c8daccfb0502100436f4
Tommy Spielhoelle <spiel.hoelle@live.de>
2018-02-24 14:53:16 +0100

    replace a element by the components render html

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,13 @@ class App extends React.Component {
    render() {
       return (
          <div>
-            Hello World!!!
+           <li className="list-group-item d-flex justify-content-between lh-condensed">
+             <div>
+               <h6 className="my-0">Product name</h6>
+               <small className="text-muted">Brief description</small>
+             </div>
+             <span className="text-muted">$12</span>
+           </li>
          </div>
       );
    }
