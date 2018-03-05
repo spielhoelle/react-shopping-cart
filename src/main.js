ae16d94011c68bc582ba690278667eec423abd51
Tommy Spielhoelle <spiel.hoelle@live.de>
2018-03-05 15:59:12 +0100

    WIP with axel

--- src/main.js
+++ src/main.js
@@ -1,3 +1,6 @@
+function requireAll (r) { r.keys().forEach(r); }
+requireAll(require.context('./', true, /\.pug$/));
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,47 @@
+const StaticSiteGeneratorPlugin = require('static-site-generator-webpack-plugin');
 var config = {
-    entry: './src/main.js',
-   output: {
-      path:'/',
-       filename: 'dist/index.js',
-   },
-   devServer: {
-      inline: true,
-      port: 8080
-   },
-   module: {
-      loaders: [
-         {
-            test: /\.jsx?$/,
-            exclude: /node_modules/,
-            loader: 'babel-loader',
-            query: {
-               presets: ['es2015', 'react']
-            }
-         }
-      ]
-   }
+  entry: './src/main.js',
+  output: {
+    path:'/',
+    filename: './dist/index.js',
+  },
+  devServer: {
+    inline: true,
+    port: 8080
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      },
+      {
+        test: /\/.pug$/,
+        // loader and use are the same
+        loader: [
+          "file-loader?name=[path][name].html",
+          "extract-loader",
+          "html-loader",
+          "pug-html-loader"
+        ],
+      }
+    ],
+  },
+  plugins: [
+    new StaticSiteGeneratorPlugin({
+      paths: [
+        '/index.pug'
+      ],
+      locals: {
+        // Properties here are merged into `locals`
+        // passed to the exported render function
+        greet: 'Hello'
+      }
+    })
+  ]
 }
 module.exports = config;
