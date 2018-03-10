5a7494db5bba0a3b2aeac154da70637b5d5246ac
Tommy Spielhoelle <spiel.hoelle@live.de>
2018-03-10 19:34:52 +0100

    pass props to header component

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,19 @@ import CheckoutForm from './CheckoutForm.jsx';
 import Header from './Header.jsx';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      headerdata: {
+          "title": "Hello from Blabla",
+          "desc": "Below is an example form built entirely with Bootstrap's form controls. Each required form group has a validation state that can be triggered by attempting to submit the form without completing it."
+        }
+    }
+  }
   render() {
     return (
       <div>
-        <Header/>
+        <Header headerdata={this.state.headerdata}/>
         <CheckoutForm/>
       </div>
     );
--- src/Header.jsx
+++ src/Header.jsx
@@ -5,11 +5,10 @@ class Header extends React.Component {
     return (
       <div className="py-5 text-center">
         <img className="d-block mx-auto mb-4" src="https://getbootstrap.com/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72"/>
-        <h2>Bootstrap</h2>
-          <p className="lead">Below is an example form built entirely with Bootstrap's form controls. Each required form group has a validation state that can be triggered by attempting to submit the form without completing it.</p>
+        <h2>{this.props.headerdata.title }</h2>
+          <p className="lead">{this.props.headerdata.desc }</p>
       </div>
       );
   }
 }
 export default Header;
-
